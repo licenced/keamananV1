76790863b0c736974e58bc56d6e66cde27650e2c
RodliyatunNichlah15 <RodliyatunNichlah15@users.noreply.github.com>
2018-11-21 05:54:37 +0700

    Form
    Update form

--- application/views/visitor/form.php
+++ application/views/visitor/form.php
@@ -2,6 +2,7 @@
 <head>
 	<style>
 	 #semua{width:600px;height:1200px;margin:auto;overflow:hidden;background:#e0a504;overflow:hidden; text-align:20px; padding:10px 20px;}
+	 #left{width:300px;height:600px; float:left;}
 	 
 	 img.tengah {
      display: block;
@@ -35,6 +36,39 @@
 	</div>
 	</div>
 	<br>
+
+	<div id="left">
+	<div class="form-group"> 
+	<img src= "<?php echo base_url().'assets/form/3. Alarm.jpg';?>" width = "200px" height = "150px">
+	<div class="checkbox">
+	<label><input type="checkbox" value="">Alarm Anti Maling</label>
+	</div>
+	<label for="jumlah"></label>
+	<div> <input type="number" min="0" id="replyNumber" data-bind="value:replyNumber" placeholder="Jumlah" />
+	</div>
+	</div>
+	<br>
+	
+	<div class="form-group"> 
+	<img src= "<?php echo base_url().'assets/form/4. GA.jpg';?>" width = "200px" height = "150px">
+	<div class="checkbox">
+	<label><input type="checkbox" value="">Gembok Alarm</label>
+	</div>
+	<label for="jumlah"></label>
+	<div> <input type="number" min="0" id="replyNumber" data-bind="value:replyNumber" placeholder="Jumlah" />
+	</div>
+	</div>
+	<br>
+	
+	<div class="form-group"> 
+	<img src="<?php echo base_url().'assets/form/5. PG.jpg';?>" width = "200px" height = "150px">
+	<div class="checkbox">
+	<label><input type="checkbox" value="">Pisau Gesper</label>
+	</div>
+	<label for="jumlah"></label>
+	<div> <input type="number" min="0" id="replyNumber" data-bind="value:replyNumber" placeholder="Jumlah" />
+	</div> <br>
+	</div>
 	
 	<div class="form-group"> 
 	<label for="alamat">Alamat</label>
