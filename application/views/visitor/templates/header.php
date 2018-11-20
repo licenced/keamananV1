da90e9979ad3e2df823053482ce087a39119e6ed
ayurahmawatii <ayurahmawatii@users.noreply.github.com>
2018-11-20 23:04:41 +0700

    header
    menambahkan header

---
+++ application/views/visitor/templates/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SecureOne</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>/assets/css/bootstrap.css">
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="<?php echo base_url();?>/assets/css/full-slider.css">
+
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="<?php echo base_url().'assets/images/logo.png'?>" width = "20"> SecureOne</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Admin</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header>
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+          <!-- Slide One - Set the background image for this slide in the line below -->
+          <div class="carousel-item active" style="background-image: url('<?php echo base_url();?>/assets/images/headerweb.png')">
+            <div class="carousel-caption d-none d-md-block">
+              <h1><!-- Welcome To Our Website --></h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
