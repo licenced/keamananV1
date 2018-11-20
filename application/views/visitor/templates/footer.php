7a7ba4030869d3e572773f70ae32adcc14f52fc8
ayurahmawatii <ayurahmawatii@users.noreply.github.com>
2018-11-21 00:04:35 +0700

    footer
    perbaikan footer

--- application/views/visitor/templates/footer.php
+++ application/views/visitor/templates/footer.php
@@ -1,11 +1,12 @@
-<!-- Footer -->
+<html>
+<body>
 <footer class="py-5 bg-dark mt-3">
-    <p class="m-0 text-center text-white">Copyright &copy; Keamanan ITERA 2018</p>
-  <!-- /.container -->
-</footer>
-
-<!-- Bootstrap core JavaScript -->
-<script src="assets/jquery/jquery-3.3.1.min.js"></script>
-<script src="assets/js/bootstrap.bundle.min.js"></script>
+	<div class="container">
+	<p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+    </div>
+    <!-- /.container -->
+    </footer>
 
 </body>
+
+</html>
