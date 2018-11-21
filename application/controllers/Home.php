9fc59903ebe34a19d0cbe96662a50df12bff6274
Doni Agus Adila <pangerancleeper@gmail.com>
2018-11-21 10:55:36 +0700

    create function view_artikel and view_tool
    membuat fungsi view_artikel, view_tools, dan meload helper, library pada fungsi construct

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,12 +6,31 @@ class Home extends CI_Controller {
   public function __construct(){
     parent::__construct();
     $this->load->model('post_model');
+    $this->load->helper('url_helper');
+    $this->load->library('upload');
   }
 
   public function index()
   {
     $data['artikel'] = $this->post_model->get_post_article();
     $this->load->view('visitor/beranda',$data);
+  }
+  public function view_artikel($slug = NULL)
+  {
+    $data['konten'] = $this->post_model->get_post($slug);
+
+
+    $this->load->view('visitor/templates/header');
+    $this->load->view('visitor/artikel', $data);
+    $this->load->view('visitor/templates/footer');
+  }
+
+  public function view_tools($slug = NULL)
+  {
+      $data['konten'] = $this->post_model->get_post($slug);
+      $this->load->view('visitor/templates/header');
+      $this->load->view('visitor/tools', $data);
+      $this->load->view('visitor/templates/footer');
   }
 
 }
