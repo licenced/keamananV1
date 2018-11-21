4a7119d2745973a6f3fc89eec80d99e8ae0b9fff
Doni Agus Adila <mragill98@gmail.com>
2018-11-21 14:04:42 +0700

    tambah fitur tambah post dan user harus login terlebih dahulu

--- application/controllers/Post.php
+++ application/controllers/Post.php
@@ -5,6 +5,9 @@ class Post extends CI_Controller {
 
   public function __construct(){
     parent::__construct();
+    if (! $this->session->userdata('masuk')) {
+      redirect('login');
+    }
     $this->load->model('post_model');
     $this->load->helper('url_helper');
     $this->load->library('upload');
@@ -48,6 +51,106 @@ class Post extends CI_Controller {
     $this->load->view('admin/index_artikel',$data);
     $this->load->view('admin/templates/footer');
 
+  }
+  public function new_post()
+  {
+    $this->load->view('admin/templates/header');
+    $this->load->view('admin/form_create');
+    $this->load->view('admin/templates/footer');
+  }
+
+
+  public function create_post()
+  {
+    echo "<script>alert('ERROR JANCOK!')</script>";
+    $config['upload_path'] = './assets/upload/img/'; //path folder
+    $config['allowed_types'] = 'gif|jpg|png|jpeg|bmp'; //type yang dapat diakses bisa anda sesuaikan
+    $config['encrypt_name'] = TRUE; //nama yang terupload nantinya
+
+    $this->upload->initialize($config);
+    if(!empty($_FILES['foto']['name'])){
+        if ($this->upload->do_upload('foto')){
+            $gbr = $this->upload->data();
+            //Compress Image
+            $config['image_library']='gd2';
+            $config['source_image']='./assets/upload/img/'.$gbr['file_name'];
+            $config['create_thumb']= FALSE;
+            $config['maintain_ratio']= FALSE;
+            $config['quality']= '100%';
+            $config['width']= 700;
+            $config['height']= 300;
+            $config['new_image']= './assets/upload/img/'.$gbr['file_name'];
+            $this->load->library('image_lib', $config);
+            $this->image_lib->resize();
+
+            $gambar=$gbr['file_name'];
+            $jdl=$this->input->post('title');
+            $kategori=$this->input->post('category');
+            $slug= url_title($jdl,'dash',TRUE);
+            $berita=$this->input->post('text');
+
+            $this->post_model->create_post($jdl,$slug,$berita,$gambar,$kategori);
+            redirect('post');
+    }else{
+      echo "<script>alert('ERROR JANCOK!')</script>";
+      redirect('post/new_post');
+
+    }
+
+    }else{
+      echo "<script>alert('ERROR JANCOK!')</script>";
+      redirect('post/new_post');
+    }
+    echo "SAMPAI SINI";
+  }
+
+  public function form_edit($id)
+  {
+    $data['konten'] = $this->post_model->get_post_id($id);
+
+    $this->load->view('admin/templates/header');
+    $this->load->view('admin/form_update',$data);
+    $this->load->view('admin/templates/footer');
+
+  }
+
+  public function edit_post(){
+    $config['upload_path'] = './assets/upload/img/'; //path folder
+    $config['allowed_types'] = 'gif|jpg|png|jpeg|bmp'; //type yang dapat diakses bisa anda sesuaikan
+    $config['encrypt_name'] = TRUE; //nama yang terupload nantinya
+
+    $this->upload->initialize($config);
+    if(!empty($_FILES['foto']['name'])){
+        if ($this->upload->do_upload('foto')){
+            $gbr = $this->upload->data();
+            //Compress Image
+            $config['image_library']='gd2';
+            $config['source_image']='./assets/upload/img/'.$gbr['file_name'];
+            $config['create_thumb']= FALSE;
+            $config['maintain_ratio']= FALSE;
+            $config['quality']= '100%';
+            $config['width']= 700;
+            $config['height']= 300;
+            $config['new_image']= './assets/upload/img/'.$gbr['file_name'];
+            $this->load->library('image_lib', $config);
+            $this->image_lib->resize();
+
+      $id = $this->input->post('id');
+      $title = $this->input->post('title');
+      $slug= url_title($title,'dash',TRUE);
+      $text=$this->input->post('text');
+      $gambar=$gbr['file_name'];
+
+      $this->post_model->update_post($id,$title,$slug,$text,$gambar);
+      redirect('post');
+      }
+    }
+
+  }
+
+  public function delete_post($id){
+    $this->post_model->delete_post($id);
+    redirect('post');
   }
 
 
