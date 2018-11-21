99a6568dfbcbfb659e0661c4fc8cf7bb336c4a3b
RodliyatunNichlah15 <RodliyatunNichlah15@users.noreply.github.com>
2018-11-21 18:50:16 +0700

    index_tools
    menambahkan index tools

--- application/views/visitor/index_tools.php
+++ application/views/visitor/index_tools.php
@@ -6,4 +6,17 @@
     return implode(" ",array_splice($words,0,$word_limit));
   } ?>
 
-  
+  <div class="row">
+  <div class="col-md-7">
+    <a href="#">
+      <img class="img-fluid rounded mb-3 mb-md-0 pl-4<!--pl-4-->" src="<?php echo base_url().'assets/upload/img/'.$konten_item['foto'];?>" alt="">
+    </a>
+  </div>
+  <div class="col-md-3">
+    <h3><?php echo $konten_item['title'];?></h3>
+    <p><small>Posted on :<?php echo $konten_item['created'] ?></small></p>
+    <p class="text-justify"><?php echo limit_words($konten_item['text'], 15)."...";?></p>
+    <a class="btn btn-primary" href="<?php echo site_url('home/view_tools/'.$konten_item['slug']); ?>">View Article</a>
+  </div>
+</div>
+<br>
